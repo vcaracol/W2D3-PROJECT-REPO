de0eaa114af174efcafd498e1f0e01f36d9bbb18
vcaracol <vcaracol@SurfacePro8.localdomain>
2022-06-22 21:55:27 -0700

    finish battleship pt 2

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -1,4 +1,11 @@
 class Board
+    
+    def self.print_grid(grid)
+        grid.each do |row|
+           puts row.join(" ")
+        end
+    end
+
     def initialize(n)
         @grid = Array.new(n) {Array.new(n,:N)}
         @size = @grid.length * @grid.length
@@ -18,5 +25,47 @@ class Board
 
     def num_ships()
         @grid.flatten.count {|ele| ele == :S}
+    end
+
+    def attack(position)
+        if self[position] == :S
+            self[position] = :H
+            puts "you sunk my battleship"
+            return true
+        else
+            self[position] = :X
+            return false
+        end
+    end
+
+    def place_random_ships
+        total_ships = @size * 0.25
+        
+        while self.num_ships < total_ships
+        rand_row = rand(0...@grid.length)
+        rand_col = rand(0...@grid.length)
+        pos = [ rand_row, rand_col ]
+        self[pos] = :S
+        end
+    end
+
+    def hidden_ships_grid
+        @grid.map do |row|
+            row.map do |ele|
+                if ele == :S
+                    :N
+                else
+                    ele
+                end
+            end
+        end
+    end
+
+    def cheat
+        Board.print_grid(@grid)
+    end
+
+    def print
+        Board.print_grid(self.hidden_ships_grid)
     end
 end
